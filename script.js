e6d693b9d8c0993530574b959616688b129010bc
vpia11 <57079920+vpia11@users.noreply.github.com>
2025-03-19 11:48:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Inicializar el mapa en una ubicación específica
+// Inicializar el mapa en una ubicación específica (Río Negro y Chubut)
 var map = L.map('map').setView([-41.1335, -67.0832], 6);
 
 // Agregar capa base de OpenStreetMap
@@ -6,7 +6,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Agregar un marcador
-L.marker([-34.6037, -58.3816]).addTo(map)
-    .bindPopup('<b>Buenos Aires</b><br>Capital de Argentina.')
-    .openPopup();
+// Marcador en Viedma (Río Negro)
+L.marker([-40.8116, -63.0031]).addTo(map)
+    .bindPopup('<b>Viedma</b><br>Capital de Río Negro.');
+
+// Marcador en Rawson (Chubut)
+L.marker([-43.3002, -65.1023]).addTo(map)
+    .bindPopup('<b>Rawson</b><br>Capital de Chubut.');
