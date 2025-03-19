782bfff1a9b2a166f18f9719d529337a1aa362b5
vpia11 <57079920+vpia11@users.noreply.github.com>
2025-03-19 12:03:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ var map = L.map('map').setView([-41.1335, -67.0832], 6);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+// Cargar datos desde un archivo GeoJSON
+fetch('datos.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            onEachFeature: function (feature, layer) {
+                if (feature.properties && feature.properties.nombre) {
+                    layer.bindPopup("<b>" + feature.properties.nombre + "</b>");
+                }
+            }
+        }).addTo(map);
+    })
+    .catch(error => console.error('Error al cargar GeoJSON:', error));
