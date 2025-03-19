0ba38f3ea56e6325d0778e01caafab90942ec95e
vpia11 <57079920+vpia11@users.noreply.github.com>
2025-03-19 11:50:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,3 @@ var map = L.map('map').setView([-41.1335, -67.0832], 6);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
-// Marcador en Viedma (Río Negro)
-L.marker([-40.8116, -63.0031]).addTo(map)
-    .bindPopup('<b>Viedma</b><br>Capital de Río Negro.');
-
-// Marcador en Rawson (Chubut)
-L.marker([-43.3002, -65.1023]).addTo(map)
-    .bindPopup('<b>Rawson</b><br>Capital de Chubut.');
